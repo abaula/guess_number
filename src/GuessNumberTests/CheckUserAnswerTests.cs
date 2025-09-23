c869f9c67e7c0256314bd20bc7dc72403aef792d
A.Baula <anton.baula@gmail.com>
2025-09-23 12:38:28 +0000

    add tests.

--- src/GuessNumberTests/CheckUserAnswerTests.cs
+++ src/GuessNumberTests/CheckUserAnswerTests.cs
@@ -8,11 +8,15 @@ namespace GuessNumberTests
         [Theory]
         [InlineData(42, 42, true)]
         [InlineData(42, 43, false)]
+        [InlineData(42, -42, false)]
+        [InlineData(42, 0, false)]
+        [InlineData(-42, -43, false)]
+        [InlineData(-42, 0, false)]
         [InlineData(0, 0, true)]
         [InlineData(-100, -100, true)]
         [InlineData(int.MaxValue, int.MaxValue, true)]
         [InlineData(int.MinValue, int.MinValue, true)]
-        public void Execute_Check(int taskAnswerValue, int correctAnswer, bool expected)
+        public void Execute_Check(int taskAnswerValue, int correctAnswer, bool expectedResult)
         {
             // Arrange
             var gameTask = new GameTask
@@ -26,7 +30,7 @@ namespace GuessNumberTests
             bool actual = checker.Execute(gameTask, correctAnswer);
 
             // Assert
-            Assert.Equal(expected, actual);
+            Assert.Equal(expectedResult, actual);
         }
     }
 }
